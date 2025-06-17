c07857ea8617be0e3c90b9c98c3b1468f78df638
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-17 05:53:55 +0100

    Add error page

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,8 @@ import homeController from "./controllers/homeController.js";
 const routes = Router();
 
 routes.use(homeController);
+routes.all('*url', (req, res) => {
+    res.render('404', { layout: false });
+});
 
 export default routes;
