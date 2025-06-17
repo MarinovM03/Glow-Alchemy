334ef7c8d8442262dbac42acd77bd37cd124772c
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-17 05:42:16 +0100

    Add routes file and move the routes there

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express, { urlencoded } from 'express';
 
-import homeController from './controllers/homeController.js';
+import routes from './routes.js';
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.use(express.static('src/public'));
 
 app.use(express.urlencoded());
 
-app.use(homeController);
+app.use(routes);
 
 app.listen(3000, console.log('Server is listening on http://localhost:3000...'));
